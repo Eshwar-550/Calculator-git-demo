8cde94d0961fc506e3aebde263e26f050847a751
Eshwar-550 <eshwareshu550@gmail.com>
2025-10-06 04:51:42 +0530

    Fixed DivisonByZero

--- Calculator.py
+++ Calculator.py
@@ -8,7 +8,10 @@ def multiply(num1,num2):
   return num1 * num2
 
 def divide(num1,num2):
-  return num1 / num2
+  if num2 == 0:
+    return "Cannot divide by Zero."
+  else:
+    return num1 / num2
 
 def calculator():
   while True:
